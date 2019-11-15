1177ac4671d4e0563a0fc3c1e866d1b4e6a1b45b
samir benzada <samirbenzada@gmail.com>
2019-11-15 11:24:42 -0500

    update

--- src/js/views/Profile/Profile.js
+++ src/js/views/Profile/Profile.js
@@ -6,8 +6,7 @@ const Profile = () => {
         <section className={styles.section}>
 
         <div class="container">
-            <h1>Edit Profile</h1>
-            <hr/>
+            <h1 className="text-center">Edit Profile</h1>
             <div class="row">
             <div class="col-md-3">
                 <div class="text-center">
@@ -19,31 +18,42 @@ const Profile = () => {
             </div>
             
             <div class="col-md-9 personal-info">
-                <h3>Personal info</h3>
                 
                 <form class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label class="col-lg-3 control-label">First name:</label>
                     <div class="col-lg-8">
-                    <input class="form-control" type="text" value="Jane" />
+                    <input class="form-control" type="text" placeholder="First name" />
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-3 control-label">Last name:</label>
                     <div class="col-lg-8">
-                    <input class="form-control" type="text" value="Bishop" />
+                    <input class="form-control" type="text" placeholder="Last name" />
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-3 control-label">Company:</label>
                     <div class="col-lg-8">
-                    <input class="form-control" type="text" value="" />
+                    <input class="form-control" type="text" placeholder="Company" />
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-lg-3 control-label">Email:</label>
                     <div class="col-lg-8">
-                    <input class="form-control" type="text" value="janesemail@gmail.com" />
+                    <input class="form-control" type="email" placeholder="Email" />
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="col-md-8">
+                    <input class="form-control" type="text" placeholder="Username" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-8">
+                    <input class="form-control" type="password" placeholder="Password"  />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-8">
+                    <input class="form-control" type="password" placeholder="Confirm password" />
                     </div>
                 </div>
                 <div class="form-group">
@@ -64,24 +74,6 @@ const Profile = () => {
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-md-3 control-label">Username:</label>
-                    <div class="col-md-8">
-                    <input class="form-control" type="text" value="janeuser" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-3 control-label">Password:</label>
-                    <div class="col-md-8">
-                    <input class="form-control" type="password" value="11111122333" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-3 control-label">Confirm password:</label>
-                    <div class="col-md-8">
-                    <input class="form-control" type="password" value="11111122333" />
-                    </div>
-                </div>
-                <div class="form-group">
                     <label class="col-md-3 control-label"></label>
                     <div class="col-md-8">
                     <input type="button" class="btn btn-primary" value="Save Changes" />
@@ -89,6 +81,7 @@ const Profile = () => {
                     <input type="reset" class="btn btn-default" value="Cancel" />
                     </div>
                 </div>
+                
                 </form>
             </div>
         </div>
