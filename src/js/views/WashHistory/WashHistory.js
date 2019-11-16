297db4a160754785f0881225def40aaefdebf0dc
samir benzada <samirbenzada@gmail.com>
2019-11-16 15:17:44 -0500

    update

--- src/js/views/WashHistory/WashHistory.js
+++ src/js/views/WashHistory/WashHistory.js
@@ -11,100 +11,37 @@ const WashHistory = () => {
 
     return (
         <section className={styles.section} style={{minHeight: windowHeight}}>
-            <div className="container">
-                <h2 className="text-center">Washing History</h2>
-                {!historyWashing ? 'Loading...' : historyWashing.map((item,index) => {
-                    var timestamp = item.start;
-                    // var ts = new Date();
-                    // console.log(ts.toDateString());
-                    var date = new Date(timestamp);
-                    var startDate = date.toDateString();
-                    // datevalues = [
-                    //    date.getFullYear(),
-                    //    date.getMonth()+1,
-                    //    date.getDate(),
-                    //    date.getHours(),
-                    //    date.getMinutes(),
-                    //    date.getSeconds(),
-                    // ];
-                    return (
-                        <div key={index} className={["alert", styles.alertBlue].join(' ')} >
-                            <div className="row text-center">
-                                <div className={["col", styles.cycle].join(' ')}> <span role="img" aria-label="dd">💧</span> {item.cicle}</div>
-                                <div className={["col", styles.time].join(' ')}> <span role="img" aria-label="dd">⏱️</span> {item.time}min</div>
-                                <div className={["col", styles.number].join(' ')}> <span role="img" aria-label="ds">#️⃣</span> {item.machineNumber}</div>
-                                <div className={["col", styles.number].join(' ')}> <span role="img" aria-label="ds">$</span> {item.price}</div>
-                                <div className={["col", styles.number].join(' ')}>  {startDate}</div>
-                                <div className={["col", styles.number].join(' ')}>  {item.cycleComplete}</div>
-                            </div>
-                            <br/>
-                        </div>
-                    )
-                })}
-            </div>
-            <hr/>
-            <br/><br/>
-            <table class="table">
+            <table class="table container">
                 <thead class="thead-dark">
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Cycle</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Number</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Complete?</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
-                </table>
 
-                <table class="table">
-                <thead class="thead-light">
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
+                {!historyWashing ? 'Loading...' : historyWashing.map((item,index) => {
+                    var timestamp = item.start;
+                    var date = new Date(timestamp);
+                    var startDate = date.toDateString();
+                    return (
+                        <tr key={index} >
+                            <th scope="row">{item.cicle}</th>
+                            <td>{item.time}</td>
+                            <td>{item.machineNumber}</td>
+                            <td>{item.price}</td>
+                            <td>{startDate}</td>
+                            <td>{item.cycleComplete}</td>
+                        </tr>
+                    )
+                })}
                 </tbody>
             </table>
+
         </section>
     );
 };
