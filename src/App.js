e678203ba3960b04d33641b6262e55ad8ebbd94c
samir benzada <samirbenzada@gmail.com>
2019-11-16 14:53:15 -0500

    update

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const CurrentWash = lazy(() => import('./js/views/CurrentWash/CurrentWash'));
 const Map = lazy(() => import('./js/views/Map/Map'));
 const Setting = lazy(() => import('./js/views/Setting/Setting'));
 const Profile = lazy(() => import('./js/views/Profile/Profile'));
+const WashHistory = lazy(() => import('./js/views/WashHistory/WashHistory'));
 const Presentation = lazy(() => import('./js/presentation/presentation'));
 
 const Rasp = lazy(() => import('./js/views/Rasp/Rasp'));
@@ -127,6 +128,7 @@ function App() {
               <Route path="/map" component={Map} />
               <Route path="/setting" component={Setting} />
               <Route path="/profile" component={Profile} />
+              <Route path="/history" component={WashHistory} />
               <Route path="/presentation" component={Presentation} />
               <Route path="/rasp" component={Rasp} />
               <Route render={() => <NotFound/>} />
